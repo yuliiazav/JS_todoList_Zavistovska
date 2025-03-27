5c0c7083ca025f5a86e24717cc1d4fccdc1ce84b
Your Nameyuliiazav <didiky1993@gmail.com>
2025-03-27 12:54:31 +0200

    added buttons and useContext

--- todo-app/src/context/TodoContext.jsx
+++ todo-app/src/context/TodoContext.jsx
@@ -68,4 +68,11 @@ export const TodoProvider = ({ children }) => {
 
   return <TodoContext.Provider value={value}> {children}</TodoContext.Provider>;
 };
-export const useTodoContext = () => useContext(TodoContext);
+
+export const useTodoContext = () => {
+  const context = useContext(TodoContext);
+  if (!context) {
+    throw new Error("useTodoContext must be used within a TodoProvider");
+  }
+  return context;
+};
